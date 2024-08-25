1b5fcad7f30e500ad97c770ac36234e0c92ae7e6
Prakash Shrestha <100356351+Prakash7676@users.noreply.github.com>
2024-08-25 20:17:27 +0545

    result part not completed

---
+++ lib/quiz/Services/api_services.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+var link = "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy";
+
+getQuiz() async {
+  var res = await http.get(Uri.parse(link));
+  if (res.statusCode == 200) {
+    var data = jsonDecode(res.body.toString());
+    print("data is loaded");
+    return data;
+  }
+}
